6008a80fa61f3a7136fe1fb1e0750fb86eb49aae
nana22022007-rgb <nana22022007@gmail.com>
2026-06-02 21:43:11 +0700

    Update BAI2.py

--- BAI2.py
+++ BAI2.py
@@ -1,3 +1,19 @@
+"""
+(1) Phân tích lỗi
+total_points được khai báo ngoài hàm nên là biến toàn cục (Global Variable).
+Python báo lỗi UnboundLocalError vì trong hàm có phép gán:
+total_points = total_points + points_earned
+nên Python tự hiểu total_points là biến cục bộ. Tuy nhiên biến này chưa được gán giá trị trước khi sử dụng nên phát sinh lỗi.
+Nếu chỉ đọc giá trị, ví dụ:
+print(total_points)
+thì chương trình không bị lỗi vì hàm được phép đọc biến toàn cục.
+Cách sửa 1: dùng từ khóa global:
+global total_points
+để Python biết rằng đang sử dụng biến toàn cục.
+Cách sửa 2 (khuyến khích): truyền tổng điểm cũ và điểm mới vào hàm, sau đó dùng:
+return
+để trả về tổng điểm sau khi cộng.
+"""
 # Bien toàn cục lưu tổng điểm hiện tại của khách hàng
 total_points = 100
 
